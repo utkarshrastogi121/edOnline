384d6f3aa2d9c2e680632d72b30a6dddf07b9a6d
utkarshrastogi121 <145201743+utkarshrastogi121@users.noreply.github.com>
2024-01-27 22:34:59 +0530

    Add files via upload

---
+++ Tailwind css/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"], // * is used for all files in dist
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
